9358d15e831f1561e519f2ba5ae6429152edb7da
Brian Bockelman <bbockelm@cse.unl.edu>
2013-04-25 15:08:04 -0500

    Fix bootstrapping of python 2.6.

--- src/python/transformation/CMSRunAnaly.sh
+++ src/python/transformation/CMSRunAnaly.sh
@@ -27,14 +27,14 @@ else
 fi
 echo "I think I found the correct CMSSW setup script"
 
-if [ -e $VO_CMS_SW_DIR/COMP/$SCRAM_ARCH/external/python/2.6.4/etc/profile.d/init.sh ]
+if [ -e $VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/python/2.6.4/etc/profile.d/init.sh ]
 then
-	. $VO_CMS_SW_DIR/COMP/$SCRAM_ARCH/external/python/2.6.4/etc/profile.d/init.sh
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$VO_CMS_SW_DIR/COMP/$SCRAM_ARCH/external/openssl/0.9.7m/lib:$VO_CMS_SW_DIR/COMP/$SCRAM_ARCH/external/bz2lib/1.0.5/lib
-elif [ -e $OSG_APP/cmssoft/cms/COMP/$SCRAM_ARCH/external/python/2.6.4/etc/profile.d/init.sh ]
+	. $VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/python/2.6.4/etc/profile.d/init.sh
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/openssl/0.9.7m/lib:$VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/bz2lib/1.0.5/lib
+elif [ -e $OSG_APP/cmssoft/cms/COMP/slc5_amd64_gcc434/external/python/2.6.4/etc/profile.d/init.sh ]
 then
-	. $OSG_APP/cmssoft/cms/COMP/$SCRAM_ARCH/external/python/2.6.4/etc/profile.d/init.sh
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OSG_APP/cmssoft/cms/COMP/$SCRAM_ARCH/external/openssl/0.9.7m/lib:$OSG_APP/cmssoft/cms/COMP/$SCRAM_ARCH/external/bz2lib/1.0.5/lib
+	. $OSG_APP/cmssoft/cms/COMP/slc5_amd64_gcc434/external/python/2.6.4/etc/profile.d/init.sh
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OSG_APP/cmssoft/cms/COMP/slc5_amd64_gcc434/external/openssl/0.9.7m/lib:$OSG_APP/cmssoft/cms/COMP/slc5_amd64_gcc434/external/bz2lib/1.0.5/lib
 fi
 command -v python2.6 > /dev/null
 rc=$?
