f054699165cdfb1b0432f1fae34a81a3b5ff4c5b
Marco Mascheroni <marco.mascheroni@cern.ch>
2013-06-20 18:51:19 +0200

    Implement MC in tweakPSet. CAF-312

--- src/python/transformation/TweakPSet.py
+++ src/python/transformation/TweakPSet.py
@@ -8,8 +8,20 @@ agentNumber = 0
 #lfnBase = '/store/temp/user/mmascher/RelValProdTTbar/mc/v6' #TODO how is this built?
 lfnBase = None
 outputMods = [] #TODO should not be hardcoded but taken from the config (how?)
-inputFiles = literal_eval(sys.argv[2])
-runAndLumis = literal_eval(sys.argv[3])
+location = sys.argv[2]
+inputFiles = literal_eval(sys.argv[3])
+runAndLumis = literal_eval(sys.argv[4])
 
-pset = SetupCMSSWPsetCore( sys.argv[1], map(str, inputFiles), runAndLumis, agentNumber, lfnBase, outputMods)
+if sys.argv[1]=='MC':
+    firstEvent=sys.argv[5]
+    lastEvent=sys.argv[6]
+    firstLumi=sys.argv[7]
+    firstRun=sys.argv[8]
+    seeding=sys.argv[9]
+    lheInputFiles=bool(literal_eval(sys.argv[10]))
+    pset = SetupCMSSWPsetCore( location, map(str, inputFiles), runAndLumis, agentNumber, lfnBase, outputMods, int(firstEvent), int(lastEvent), int(firstLumi),\
+                    int(firstRun), seeding, lheInputFiles)
+else:
+    pset = SetupCMSSWPsetCore( location, map(str, inputFiles), runAndLumis, agentNumber, lfnBase, outputMods)
+
 pset()
